3100bc8c8abb0d1f5f0eaf8aa5ae0674a25274ca
Eirini Ntzani <eirinintzani2@gmail.com>
2025-12-11 19:07:13 +0200

    Update info.php

--- week-01/info.php
+++ week-01/info.php
@@ -1,6 +1,4 @@
 <?php
-// The phpinfo() function displays detailed information
-// about the PHP configuration on your server
-// (version, extensions, environment variables, etc.)
+// Display detailed information about the current PHP configuration
 phpinfo();
 ?>
