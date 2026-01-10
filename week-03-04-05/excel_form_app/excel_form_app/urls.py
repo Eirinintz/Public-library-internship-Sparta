e32a61c81281bc5274fefcf22f97db5439135729
Eirini Ntzani <eirinintzani2@gmail.com>
2026-01-10 16:09:59 +0200

    Update urls.py

--- week-03-04-05/excel_form_app/excel_form_app/urls.py
+++ week-03-04-05/excel_form_app/excel_form_app/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include  # make sure to import include
+from django.contrib import admin  # Import Django admin site
+from django.urls import path, include  # Import path for URL routing and include for app URLs
 
+# Define the URL patterns for the project
 urlpatterns = [
+    # Admin panel URL
     path('admin/', admin.site.urls),
+
+    # Include URLs from the "main" application
     path('', include('main.urls')),  # include your app URLs
 ]
