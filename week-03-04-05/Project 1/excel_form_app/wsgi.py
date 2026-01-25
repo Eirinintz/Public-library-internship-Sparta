669cc2507e71a6ec7f1ad44d3bf444f169fa22df
Eirini Ntzani <eirinintzani2@gmail.com>
2026-01-25 17:12:07 +0200

    Update wsgi.py

--- week-03-04-05/Project 1/excel_form_app/wsgi.py
+++ week-03-04-05/Project 1/excel_form_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for excel_form_app project.
+WSGI config for Project 1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'excel_form_app.settings')
 
 application = get_wsgi_application()
+
